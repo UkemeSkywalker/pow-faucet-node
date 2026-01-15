0111497da5276e2b3fe9dab8b34f2ae39a61abcd
UkemeSkywalker <skywalkershadow9@gmail.com>
2026-01-15 04:38:28 +0100

    config faucet-config.yaml

--- infra/main.tf
+++ infra/main.tf
@@ -16,5 +16,5 @@ module "pow_faucet_node" {
   key_name           = aws_key_pair.deployer.key_name
   vpc_id             = module.vpc.vpc_id
   subnet_id          = module.vpc.public_subnet_id
-  allowed_tcp_ports  = [8080]
+  allowed_tcp_ports  = [8080, 9090, 3000]
 }
